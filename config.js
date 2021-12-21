c47341f77f4d0637efed04cd2a335959c04b3467
MuhammadBilal <bilalzafarr39@gmail.com>
2021-12-21 18:50:45 +0500

    updateeeee

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-module.exports = {
-  port: 8000,
-  db: {
-    prod: "mongodb+srv://muhammadbilal:gSVYcdEmbGoMAKJ4@cluster0.htpyq.mongodb.net/survey?retryWrites=true&w=majority",
+// module.exports = {
+//   port: 8000,
+//   db: {
+//     prod: "mongodb+srv://muhammadbilal:gSVYcdEmbGoMAKJ4@cluster0.htpyq.mongodb.net/survey?retryWrites=true&w=majority",
 
-    options: {
-      useNewUrlParser: true,
-    },
-  },
-  jwt: {
-    expiry: "7d",
-  },
-};
+//     options: {
+//       useNewUrlParser: true,
+//     },
+//   },
+//   jwt: {
+//     expiry: "7d",
+//   },
+// };
